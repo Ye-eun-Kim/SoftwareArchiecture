63b1c33ef9c062a91b8d07955fec50b4ae00e150
Ye-eun-Kim <yk23aao@herts.ac.uk>
2023-12-30 04:19:38 +0000

    change the comment into English

--- src/CompetitorManagerGUI.java
+++ src/CompetitorManagerGUI.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.List;
 
@@ -21,22 +19,23 @@ public class CompetitorManagerGUI {
         initialize();
     }
 
+
     private void initialize() {
         frame = new JFrame("Competitor Manager");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        // 세 개의 주요 패널을 포함하는 메인 패널 생성
+        // Create main panel containing three primary panels
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)); // 수직 정렬
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)); // Vertical alignment
         mainPanel.add(createScorePanel());
         mainPanel.add(createTablePanel());
         mainPanel.add(createDetailsPanel());
 
-        // 메인 패널을 프레임 중앙에 배치
+        // Place main panel in the center of the frame
         frame.add(mainPanel, BorderLayout.CENTER);
 
-        // 닫기 버튼을 포함하는 하단 패널 생성 및 추가
+        // Create and add bottom panel containing the close button
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JButton closeButton = new JButton("Close");
         closeButton.addActionListener(e -> {
@@ -45,10 +44,10 @@ public class CompetitorManagerGUI {
         });
         bottomPanel.add(closeButton);
 
-        // 하단 패널을 프레임 하단에 배치
+        // Place bottom panel at the bottom of the frame
         frame.add(bottomPanel, BorderLayout.PAGE_END);
 
-        // 프레임 크기 설정 및 표시
+        // Set frame size and make it visible
         frame.pack();
         frame.setVisible(true);
     }
@@ -63,6 +62,7 @@ public class CompetitorManagerGUI {
         JTextField scoresField = new JTextField(20);
         JButton updateButton = new JButton("Update Scores");
 
+        // Display scores when 'View Scores' button is clicked
         viewButton.addActionListener(e -> {
             int id = Integer.parseInt(competitorIdField.getText());
             Competitor competitor = competitorList.findCompetitor(id);
@@ -73,22 +73,23 @@ public class CompetitorManagerGUI {
             }
         });
 
+        // Update scores when 'Update Scores' button is clicked
         updateButton.addActionListener(e -> {
             try {
                 int id = Integer.parseInt(competitorIdField.getText());
                 Competitor competitor = competitorList.findCompetitor(id);
                 if (competitor != null) {
-                    // 새로운 점수 배열을 Competitor 객체에 반영
+                    // Reflect new score array in Competitor object
                     int[] updatedScores = parseScores(scoresField.getText());
                     competitor.setScoreArray(updatedScores);
 
-                    // 전체 점수를 재계산
+                    // Recalculate total score
                     double newOverallScore = competitor.getOverallScore();
 
-                    // 사용자에게 새로운 전체 점수 알림
+                    // Notify user of new total score
                     JOptionPane.showMessageDialog(frame, "Scores updated. New overall score: " + newOverallScore);
 
-                    // 테이블 정보 업데이트
+                    // Update table information
                     updateTableModel(null);
                 } else {
                     JOptionPane.showMessageDialog(frame, "Competitor not found.");
@@ -107,7 +108,7 @@ public class CompetitorManagerGUI {
         return panel;
     }
 
-
+    // Parse score text to an array of scores
     private int[] parseScores(String scoresText) {
         String[] scoreStrings = scoresText.trim().split("\\s*,\\s*");
         int[] scores = new int[scoreStrings.length];
@@ -123,26 +124,25 @@ public class CompetitorManagerGUI {
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
         table = new JTable(model);
 
-        // 경쟁자 데이터로 테이블 채우기
+        // Fill the table with competitor data
         for (Competitor competitor : competitorList.getAllCompetitors()) {
             model.addRow(new Object[]{competitor.getId(), competitor.getFullName(), competitor.getCategory(), competitor.getScoreString(), competitor.getOverallScore()});
         }
 
-        // 정렬 옵션 및 버튼을 담을 새로운 패널 생성
-        JPanel sortPanel = new JPanel(); // FlowLayout이 기본값
+        // Create new panel for sorting options and button
+        JPanel sortPanel = new JPanel(); // Default FlowLayout
         JComboBox<String> sortOptions = new JComboBox<>(new String[]{"ID", "Name", "Category", "Scores", "Overall Score"});
         JButton sortButton = new JButton("Sort");
 
+        // Sort list based on selected criteria
         sortButton.addActionListener(e -> {
             String selectedOption = (String) sortOptions.getSelectedItem();
             sortCompetitorList(selectedOption, model);
         });
 
-        // 정렬 옵션 및 버튼을 패널에 추가
         sortPanel.add(sortOptions);
         sortPanel.add(sortButton);
 
-        // 패널을 BorderLayout.NORTH에 추가
         panel.add(sortPanel, NORTH);
         panel.add(new JScrollPane(table), BorderLayout.CENTER);
 
@@ -176,7 +176,7 @@ public class CompetitorManagerGUI {
         List<Competitor> sortedCompetitors = new ArrayList<>(competitorList.getAllCompetitors());
         Collections.sort(sortedCompetitors, comparator);
 
-        // 업데이트된 정렬된 목록을 테이블 모델에 반영
+        // Reflect updated sorted list in table model
         updateTableModel(sortedCompetitors);
     }
 
@@ -184,7 +184,7 @@ public class CompetitorManagerGUI {
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0); // 테이블 데이터 클리어
 
-        // 정렬된 목록이 제공되면 해당 목록 사용, 아니면 기본 목록 사용
+        // If a sorted list is provided, use that list, otherwise use the default list.
         List<Competitor> competitors = (sortedCompetitors != null) ? sortedCompetitors : competitorList.getAllCompetitors();
 
         for (Competitor competitor : competitors) {
@@ -203,8 +203,7 @@ public class CompetitorManagerGUI {
     private JPanel createDetailsPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-
-        // 상단: 경쟁자 정보 조회
+        // Top: Look up competitor information
         JPanel topPanel = new JPanel();
         JLabel label = new JLabel("Competitor ID:");
         JTextField competitorIdField = new JTextField(10);
@@ -213,19 +212,19 @@ public class CompetitorManagerGUI {
         topPanel.add(competitorIdField);
         topPanel.add(viewButton);
 
-        // 중앙: 세부 정보 표시
+        // center: show details
         JTextArea detailsArea = new JTextArea(5, 30);
-        detailsArea.setEditable(false); // 기본적으로 편집 불가
+        detailsArea.setEditable(false); // Editing is not possible by default
 
-        // 하단: 세부 정보 수정
+        // bottom: edit details
         JPanel bottomPanel = new JPanel();
         JButton editButton = new JButton("Edit Details");
         JButton saveButton = new JButton("Save Changes");
-        saveButton.setEnabled(false); // 처음에는 비활성화
+        saveButton.setEnabled(false); // initially disabled
 
         editButton.addActionListener(e -> {
-            detailsArea.setEditable(true); // 편집 가능하도록 설정
-            saveButton.setEnabled(true); // 저장 버튼 활성화
+            detailsArea.setEditable(true); // Make it editable
+            saveButton.setEnabled(true); // Activate save button
         });
 
         viewButton.addActionListener(e -> {
@@ -233,7 +232,7 @@ public class CompetitorManagerGUI {
                 int id = Integer.parseInt(competitorIdField.getText());
                 Competitor competitor = competitorList.findCompetitor(id);
                 if (competitor != null) {
-                    // 세부 정보를 텍스트 영역에 표시
+                    // Display details in text area
                     detailsArea.setText(competitor.getFullDetails());
                 } else {
                     JOptionPane.showMessageDialog(frame, "Competitor not found.");
@@ -248,10 +247,9 @@ public class CompetitorManagerGUI {
                 int id = Integer.parseInt(competitorIdField.getText());
                 Competitor competitor = competitorList.findCompetitor(id);
                 if (competitor != null) {
-                    // detailsArea의 텍스트로 경쟁자 정보 업데이트 로직 구현 필요
                     updateCompetitorDetails(competitor, detailsArea.getText());
-                    detailsArea.setEditable(false); // 편집 불가능하도록 설정
-                    saveButton.setEnabled(false); // 저장 버튼 비활성화
+                    detailsArea.setEditable(false);
+                    saveButton.setEnabled(false);
                     JOptionPane.showMessageDialog(frame, "Details updated.");
                 } else {
                     JOptionPane.showMessageDialog(frame, "Competitor not found.");
@@ -264,7 +262,6 @@ public class CompetitorManagerGUI {
         bottomPanel.add(editButton);
         bottomPanel.add(saveButton);
 
-        // 패널 구성
         panel.add(topPanel, NORTH);
         panel.add(new JScrollPane(detailsArea), BorderLayout.CENTER);
         panel.add(bottomPanel, SOUTH);
@@ -273,12 +270,12 @@ public class CompetitorManagerGUI {
     }
 
     private void updateCompetitorDetails(Competitor competitor, String details) {
-        // 세부 정보 파싱 로직
+        // Detailed information parsing logic
         String[] lines = details.split("\n");
 
-        // 첫 번째 줄 처리 (경쟁자 번호, 이름, 나이, 카테고리)
+        // Process first line (competitor number, name, age, category)
         String[] firstLineDetails = lines[0].split(", ");
-        String name = firstLineDetails[1].substring(6); // "Name: " 이후의 문자열
+        String name = firstLineDetails[1].substring(6);  // String after "Name: "
         int age = 0;
         try {
             age = Integer.parseInt(firstLineDetails[2].substring(firstLineDetails[2].indexOf(": ") + 2));
@@ -287,16 +284,16 @@ public class CompetitorManagerGUI {
             return;
         }
         String category = firstLineDetails[3].substring(firstLineDetails[3].indexOf(": ") + 2);
-        // 유효한 카테고리 목록
+        // list of valid categories
         List<String> validCategories = Arrays.asList("Freestyle", "Butterfly");
         if (!validCategories.contains(category)) {
             JOptionPane.showMessageDialog(frame, "Invalid category. Please enter a valid category.");
             return;
         }
 
-        // 두 번째 줄 처리 (점수)
+        // Process second line (score)
         String[] secondLineDetails = lines[1].split(", ");
-        String[] scoreStrings = secondLineDetails[0].substring(8).split(","); // "Scores: " 이후의 문자열을 쉼표로 분리
+        String[] scoreStrings = secondLineDetails[0].substring(8).split(","); // Separate the string after "Scores: " with commas
         int[] scoreValues = new int[scoreStrings.length];
         try {
             for (int i = 0; i < scoreStrings.length; i++) {
@@ -307,13 +304,13 @@ public class CompetitorManagerGUI {
             return;
         }
 
-        // 세부 정보 업데이트
+        // update details
         competitor.setName(name);
         competitor.setAge(age);
         competitor.setCategory(category);
         competitor.setScoreArray(scoreValues);
 
-        // 테이블 업데이트
+        // update table
         updateTableModel(null);
     }
 
